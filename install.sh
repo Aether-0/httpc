f32e2af7c4038cfc0b213451b96f93595c7e29fc
Aether-0 <143575444+Aether-0@users.noreply.github.com>
2024-05-10 23:44:51 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,9 @@
 
 # Define variables for file paths
 LOCAL_SCRIPT_FILE="httpc.py"
-REMOTE_SCRIPT_URL="https://raw.githubusercontent.com/Aether-0/httpc/main/httpc.py"
 INSTALL_PATH="/bin/httpc"
 
-# Check if the local script file exists
-if [ ! -f "$LOCAL_SCRIPT_FILE" ]; then
-    echo "Local script file '$LOCAL_SCRIPT_FILE' not found. Attempting to download from $REMOTE_SCRIPT_URL..."
     
-    # Download the script file from the remote URL
-    wget -q "$REMOTE_SCRIPT_URL" -O "$LOCAL_SCRIPT_FILE"
-    
-    # Check if the download was successful
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to download the script from '$REMOTE_SCRIPT_URL'."
-        exit 1
-    fi
-fi
 
 # Check if sudo privileges are available
 if [ "$(id -u)" -ne 0 ]; then
